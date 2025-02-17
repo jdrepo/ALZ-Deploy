c831710951bfd772b8ef7a0ec9b1d025040ba63d
Jens Diedrich <jens.diedrich@googlemail.com>
2025-02-17 12:02:14 +0100

    var test

--- opnsense-onprem/scripts/configureopnsense.sh
+++ opnsense-onprem/scripts/configureopnsense.sh
@@ -37,7 +37,7 @@ elif [ "$4" = "TwoNics" ]; then
     fetch $1get_nic_gw.py
     gwip=$(python get_nic_gw.py $5)
     sed -i "" "s/yyy.yyy.yyy.yyy/$gwip/" config.xml
-    sed -i "" "s/aaa.aaa.aaa.aaa/$3/" config.xml
+    sed -i "" "s/aaa.aaa.aaa.aaa/$6/" config.xml
     # sed -i "" "s_zzz.zzz.zzz.zzz_$6_" config.xml
     cp config.xml /usr/local/etc/config.xml
 fi
