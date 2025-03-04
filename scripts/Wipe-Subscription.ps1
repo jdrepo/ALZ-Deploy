7c6397003de4998bbda2517d408aa0ddb1d84ed2
Jens Diedrich <jens.diedrich@googlemail.com>
2025-03-04 11:47:54 +0100

    add principal id output

--- scripts/Wipe-Subscription.ps1
+++ scripts/Wipe-Subscription.ps1
@@ -93,6 +93,8 @@ function Invoke-RemoveOrphanedRoleAssignment {
     $principalsResponse = Invoke-AzRestMethod -Method "POST" -Uri $principalsRequestUri -Payload $principalsRequestBody -WhatIf:$false
     $principalIds = ($principalsResponse.Content | ConvertFrom-Json).value.id
 
+     Write-Host "Principal IDs: $($principalIds)"
+
     # Find all Role Assignments where the principalId is not found in AAD
     $orphanedRoleAssignments = $roleAssignments | Where-Object {
             ($_.properties.scope -eq "/subscriptions/$($SubscriptionId)") -and
