99a9335a270e877231b2f11404420fec99268cd0
Jens Diedrich <jens.diedrich@googlemail.com>
2025-05-10 16:51:11 +0200

    .

--- opnsense-onprem/scripts/configureopnsense-vpngw-bgp.sh
+++ opnsense-onprem/scripts/configureopnsense-vpngw-bgp.sh
@@ -19,18 +19,6 @@ if [ "$4" = "vpngw-bgp" ]; then
     cp config-vpngw-bgp.xml /usr/local/etc/config.xml
 fi
 
-
-#OPNSense default configuration template
-#fetch https://raw.githubusercontent.com/dmauser/opnazure/dev_active_active/scripts/$1
-#fetch https://raw.githubusercontent.com/dmauser/opnazure/master/scripts/$1
-#cp $1 /usr/local/etc/config.xml
-
-# 1. Package to get root certificate bundle from the Mozilla Project (FreeBSD)
-# 2. Install bash to support Azure Backup integration
-#env IGNORE_OSVERSION=yes
-#pkg bootstrap -f; pkg update -f
-#env ASSUME_ALWAYS_YES=YES pkg install ca_root_nss && pkg install -y bash
-
 #Download OPNSense Bootstrap and Permit Root Remote Login
 fetch https://raw.githubusercontent.com/opnsense/update/master/src/bootstrap/opnsense-bootstrap.sh.in
 sed -i "" 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
@@ -44,25 +32,11 @@ sed -i "" "s/set -e/#set -e/g" opnsense-bootstrap.sh.in
 sed -i "" "s/reboot/shutdown -r +2/g" opnsense-bootstrap.sh.in
 sh ./opnsense-bootstrap.sh.in -y -r "$2"
 
-Add Azure waagent
-fetch https://github.com/Azure/WALinuxAgent/archive/refs/tags/v$3.tar.gz
-tar -xvzf v$3.tar.gz
-cd WALinuxAgent-$3/
-python3 setup.py install --register-service --lnx-distro=freebsd --force
-cd ..
-
-# Fix waagent by replacing configuration settings
-ln -s /usr/local/bin/python3.11 /usr/local/bin/python
-# ##sed -i "" 's/command_interpreter="python"/command_interpreter="python3"/' /etc/rc.d/waagent
-# ##sed -i "" 's/#!\/usr\/bin\/env python/#!\/usr\/bin\/env python3/' /usr/local/sbin/waagent
-sed -i "" 's/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=n/' /etc/waagent.conf
-fetch $1actions_waagent.conf
-cp actions_waagent.conf /usr/local/opnsense/service/conf/actions.d
-
 # Installing bash - This is a requirement for Azure custom Script extension to run
 pkg install -y bash
 pkg install -y os-frr
 pkg install -y os-ddclient
+pkg install -y azure-agent
 
 # Remove wrong route at initialization
 cat > /usr/local/etc/rc.syshook.d/start/22-remoteroute <<EOL
